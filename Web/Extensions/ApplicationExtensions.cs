6c65b08869a2a17b479ca866bc1be62b4daa03de
Amritendu Panja <amritendu.panja@gmail.com>
2023-09-05 18:02:39 +0530

    Moved dependencies to extension classes. Code cleanups. #17

--- Web/Extensions/ApplicationExtensions.cs
+++ Web/Extensions/ApplicationExtensions.cs
@@ -1,9 +1,75 @@
-﻿using Web.Middlewares;
+﻿using Application.Helpers;
+using Application.Repository;
+using Common.Entities;
+using Common.Mappings;
+using Common.Settings;
+using Infrastructure.Data.ApplicationDbContext.Repositories;
+using Infrastructure.Data.Repository;
+using Infrastructure.Helpers;
+using Microsoft.AspNetCore.Authentication.JwtBearer;
+using Microsoft.IdentityModel.Tokens;
+using System.Reflection;
+using Web.Middlewares;
 
 namespace Web.Extensions
 {
     public static class ApplicationExtensions
     {
+        public static IServiceCollection AddJwtAuthentication(this IServiceCollection services, AppSettings settings)
+        {
+            services.AddAuthentication(JwtBearerDefaults.AuthenticationScheme).AddJwtBearer(o =>
+            {
+                o.TokenValidationParameters = new TokenValidationParameters
+                {
+                    IssuerSigningKey = new SymmetricSecurityKey(System.Text.Encoding.UTF8.GetBytes(settings.Security.Authentication.AccessTokenSecret)),
+                    ValidIssuer = settings.Security.Authentication.Issuer,
+                    ValidAudience = settings.Security.Authentication.Audience,
+                    ValidateIssuerSigningKey = true,
+                    ValidateIssuer = true,
+                    ValidateAudience = true,
+                    ClockSkew = TimeSpan.Zero
+                };
+                o.Events = new JwtBearerEvents
+                {
+                    OnAuthenticationFailed = context =>
+                    {
+                        if (context.Exception.GetType() == typeof(SecurityTokenExpiredException))
+                        {
+                            context.Response.Headers.Add("IS-TOKEN-EXPIRED", "true");
+                        }
+                        return Task.CompletedTask;
+                    }
+                };
+            });
+            return services;
+        }
+
+        public static IServiceCollection AddRepositories(this IServiceCollection services)
+        {
+            services.AddTransient<IApplicationUserRepository, ApplicationUserRepository>();
+            services.AddTransient<IRepository<ApplicationLayer>, Repository<ApplicationLayer>>();
+            services.AddTransient<IRepository<SecurityTokenLog>, Repository<SecurityTokenLog>>();
+            return services;
+        }
+
+        public static IServiceCollection AddMediatorSettings(this IServiceCollection services)
+        {
+            services.AddMediatR(cfg => cfg.RegisterServicesFromAssemblies(
+                Assembly.GetExecutingAssembly(),
+                typeof(Infrastructure.MediatorDependency.MediatorStartup).Assembly,
+                typeof(Application.MediatorDependency.MediatorStartup).Assembly,
+                typeof(Common.MediatorDependency.MediatorStartup).Assembly));
+            return services;
+        }
+
+        public static IServiceCollection AddHelpers(this IServiceCollection services)
+        {
+            services.AddTransient<SharedMapping>();
+            services.AddTransient<IHashHelper, HashHelper>();
+            services.AddTransient<ITokenHelper, JwtTokenHelper>();
+            return services;
+        }
+
         public static IApplicationBuilder UseGlobalExceptionHandler(this IApplicationBuilder app)
         {
             app.UseMiddleware<ErrorHandlingMiddleware>();
--- Web/Program.cs
+++ Web/Program.cs
@@ -1,70 +1,30 @@
-using Application.Helpers;
-using Application.Repository;
-using Common.Entities;
-using Common.Mappings;
 using Common.Settings;
 using Infrastructure.Data.ApplicationDbContext;
-using Infrastructure.Data.ApplicationDbContext.Repositories;
-using Infrastructure.Data.Repository;
-using Infrastructure.Helpers;
-using Microsoft.AspNetCore.Authentication.JwtBearer;
 using Microsoft.EntityFrameworkCore;
-using Microsoft.IdentityModel.Tokens;
 using NLog.Web;
-using System.Reflection;
 using Web.Extensions;
-using Web.Middlewares;
 
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
 builder.Services.AddDbContext<ApplicationDbContext>(options => options.UseNpgsql(builder.Configuration.GetConnectionString("DBConnection")));
 
-builder.Services.AddTransient<SharedMapping>();
-builder.Services.AddTransient<IHashHelper, HashHelper>();
-builder.Services.AddTransient<ITokenHelper, JwtTokenHelper>();
-builder.Services.AddTransient<IApplicationUserRepository, ApplicationUserRepository>();
-builder.Services.AddTransient<IRepository<ApplicationLayer>, Repository<ApplicationLayer>>();
-builder.Services.AddTransient<IRepository<SecurityTokenLog>, Repository<SecurityTokenLog>>();
+builder.Services.AddHelpers();
+builder.Services.AddRepositories();
 
 builder.Services.Configure<AppSettings>(builder.Configuration.GetSection("AppSettings"));
 
-builder.Services.AddMediatR(cfg => cfg.RegisterServicesFromAssemblies(
-    Assembly.GetExecutingAssembly(),
-    typeof(Infrastructure.MediatorDependency.MediatorStartup).Assembly,
-    typeof(Application.MediatorDependency.MediatorStartup).Assembly,
-    typeof(Common.MediatorDependency.MediatorStartup).Assembly));
+builder.Services.AddMediatorSettings();
 
 builder.Logging.ClearProviders();
 builder.Host.UseNLog();
 
 builder.Services.AddControllers();
 
-builder.Services.AddAuthentication(JwtBearerDefaults.AuthenticationScheme).AddJwtBearer(o =>
-{
-    AppSettings settings = new AppSettings();
-    builder.Configuration.Bind("AppSettings", settings);
-    o.TokenValidationParameters = new TokenValidationParameters
-    {
-        IssuerSigningKey = new SymmetricSecurityKey(System.Text.Encoding.UTF8.GetBytes(settings.Security.Authentication.AccessTokenSecret)),
-        ValidIssuer = settings.Security.Authentication.Issuer,
-        ValidAudience = settings.Security.Authentication.Audience,
-        ValidateIssuerSigningKey = true,
-        ValidateIssuer = true,
-        ValidateAudience = true,
-        ClockSkew = TimeSpan.Zero
-    };
-    o.Events = new JwtBearerEvents
-    {
-        OnAuthenticationFailed = context => {
-            if (context.Exception.GetType() == typeof(SecurityTokenExpiredException))
-            {
-                context.Response.Headers.Add("IS-TOKEN-EXPIRED", "true");
-            }
-            return Task.CompletedTask;
-        }
-    };
-});
+AppSettings settings = new AppSettings();
+builder.Configuration.Bind("AppSettings", settings);
+
+builder.Services.AddJwtAuthentication(settings);
 
 // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
 builder.Services.AddEndpointsApiExplorer();
