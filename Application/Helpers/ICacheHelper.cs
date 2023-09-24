18bbb19bb98fa1d5d9f5c63efd976c1833a78101
Amritendu Panja <amritendu.panja@gmail.com>
2023-09-24 18:46:52 +0530

    Data cache implementation start #2

---
+++ Application/Helpers/ICacheHelper.cs
@@ -0,0 +1,9 @@
+﻿namespace Application.Helpers
+{
+    public interface ICacheHelper
+    {
+        void Set<T>(string key, T value);
+        T Get<T>(string key);
+        void Remove(string key);
+    }
+}
---
+++ Infrastructure/Helpers/CacheHelper.cs
@@ -0,0 +1,41 @@
+﻿using Application.Helpers;
+using Common.Settings;
+using Microsoft.Extensions.Caching.Memory;
+using Microsoft.Extensions.Options;
+
+namespace Infrastructure.Helpers
+{
+    public class CacheHelper : ICacheHelper
+    {
+        private readonly IMemoryCache memoryCache;
+        private readonly AppSettings appSettings;
+
+        public CacheHelper(IMemoryCache memoryCache, IOptions<AppSettings> options)
+        {
+            this.memoryCache = memoryCache;
+            appSettings = options.Value;
+        }
+
+        public void Set<T>(string key, T value)
+        {
+            Remove(key);
+            var entryOptions = new MemoryCacheEntryOptions();
+            entryOptions.SetSlidingExpiration(TimeSpan.FromMinutes(appSettings.Cache.TimeoutInMinutes));
+            memoryCache.Set<T>(key, value, entryOptions);
+        }
+
+        public T Get<T>(string key)
+        {
+            memoryCache.TryGetValue<T>(key, out var value);
+            return value;
+        }
+
+        public void Remove(string key)
+        {
+            if (memoryCache.TryGetValue(key, out var value))
+            {
+                memoryCache.Remove(key);
+            }
+        }
+    }
+}
